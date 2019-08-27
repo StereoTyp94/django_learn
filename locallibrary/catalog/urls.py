1c0c3a152f2ba64d541b667d73f0da1c49596c6e
StereoTyp94 <alvin325@mail.ru>
2019-08-27 19:32:23 +0300

    Page MyBorrowed and page All Borrowed Books

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('author/', views.AuthorListView.as_view(), name='author'),
     path('author/<int:pk>/', views.AuthorDetailView.as_view(), name='author-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('borredbooks/', views.AllBorrowedBooks.as_view(), name='all-borrowed'),
 ]
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -46,3 +46,12 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+
+class AllBorrowedBooks(LoginRequiredMixin, generic.ListView):
+    model = BookInstance
+    template_name = 'catalog/borrowed_books.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
